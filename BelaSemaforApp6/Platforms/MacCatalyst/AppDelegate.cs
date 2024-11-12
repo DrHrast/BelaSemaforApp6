a3cf8b286dd01a136792624671c97044ffbc4cfa
DrHrast <petar.huljek@gmail.com>
2024-11-12 10:20:07 +0100

    Add project files.

---
+++ BelaSemaforApp6/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BelaSemaforApp6
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
