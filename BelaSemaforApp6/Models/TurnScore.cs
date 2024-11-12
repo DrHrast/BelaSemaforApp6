e289e3cd541ee7023629e2e3417c7df81c8eeff4
DrHrast <petar.huljek@gmail.com>
2024-11-12 14:35:33 +0100

    Implemented styles for xaml components.

--- BelaSemaforApp6/Models/TurnScore.cs
+++ BelaSemaforApp6/Models/TurnScore.cs
@@ -8,30 +8,11 @@ public class TurnScore
     public int TeamTwo { get; set; }
     public bool TwoBela { get; set; }
     public int TwoCall { get; set; }
-    public int TeamOneTotal { get; set; }
-    public int TeamTwoTotal { get; set; }
+    public int TeamOneTotal { get; }
+    public int TeamTwoTotal { get; }
 
     public TurnScore()
     {
-        TeamScoreAddition();
-    }
-
-    private void TeamScoreAddition()
-    {
-        if (OneBela)
-        {
-            TeamOneTotal = TeamOne + 20 + OneCall;
-            TeamTwoTotal = TeamTwo + TwoCall;
-        }
-        else if (TwoBela)
-        {
-            TeamOneTotal = TeamOne + OneCall;
-            TeamTwoTotal = TeamTwo + 20 + TwoCall;
-        }
-        else
-        {
-            TeamOneTotal = TeamOne + OneCall;
-            TeamTwoTotal = TeamTwo + TwoCall;
-        }
+       
     }
 }
