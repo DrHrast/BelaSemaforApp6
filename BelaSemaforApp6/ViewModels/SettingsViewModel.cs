f281b082918b6b23926e046385fd64f6857a72b5
DrHrast <petar.huljek@gmail.com>
2024-11-15 14:24:50 +0100

    Some tweaks to settings and main views.

--- BelaSemaforApp6/ViewModels/SettingsViewModel.cs
+++ BelaSemaforApp6/ViewModels/SettingsViewModel.cs
@@ -1,8 +1,16 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Configuration;
 
 namespace BelaSemaforApp6.ViewModels;
 
 partial class SettingsViewModel : ObservableObject
 {
-    
+    public SettingsViewModel(IConfiguration config) {}
+
+    [RelayCommand]
+    private async void NavigateToGame()
+    {
+        await Shell.Current.GoToAsync("///GameView");
+    }
 }
