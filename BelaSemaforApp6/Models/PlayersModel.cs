3f0bd064e5ed87cc591414e7a8bf3a624e1e33ec
DrHrast <petar.huljek@gmail.com>
2024-11-20 09:55:40 +0100

    Added PlayersModel and TeamsModel.

---
+++ BelaSemaforApp6/Models/PlayersModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BelaSemaforApp6.Models;
+
+public class PlayersModel
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
---
+++ BelaSemaforApp6/Models/TeamsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BelaSemaforApp6.Models;
+
+public class TeamsModel
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int PlayerOneId { get; set; }
+    public int PlayerTwoId { get; set; }
+}
