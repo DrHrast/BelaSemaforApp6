53654282afa52c4b29eb12e97a31d49bc0dd3aa0
DrHrast <petar.huljek@gmail.com>
2024-12-05 12:05:29 +0100

    Changed theme colors to be more soft and pastel.

--- BelaSemaforApp6/ViewModels/SettingsViewModel.cs
+++ BelaSemaforApp6/ViewModels/SettingsViewModel.cs
@@ -17,30 +17,30 @@ partial class SettingsViewModel : ObservableObject
     [ObservableProperty] private bool _canAddPlayer = false;
     [ObservableProperty] private ObservableCollection<Color> _primaryColors =
     [
-        Color.Parse("#fdfdfd"),
-        Color.Parse("#696969"),
-        Color.Parse("#181818"),
+        Color.Parse("#FFFFFF"), // Soft White
+        Color.Parse("#A8A8A8"), // Light Gray
+        Color.Parse("#303030"), // Charcoal Gray
     ];
     [ObservableProperty] private ObservableCollection<Color> _secondaryColors =
     [
-        Color.Parse("#000000"),
-        Color.Parse("#FFFFFF"),
+        Color.Parse("#F5F5F5"), // Soft White
         Color.Parse("#D3D3D3"), // Light Gray
-        Color.Parse("#708090"), // Slate Gray
-        Color.Parse("#2F2F2F"), // Dark Gray
-        Color.Parse("#D8BFD8"), // Thistle
-        Color.Parse("#FF69B4"), // Hot Pink
-        Color.Parse("#8A0303"), // Blood Red
-        Color.Parse("#8B0000"), // Dark Red
-        Color.Parse("#FF4500"), // Orange Red
-        Color.Parse("#FFD700"), // Gold
-        Color.Parse("#FFFF00"), // Yellow
-        Color.Parse("#228B22"), // Forest Green
-        Color.Parse("#2E8B57"), // Sea Green
-        Color.Parse("#00CED1"), // Dark Turquoise
-        Color.Parse("#1E90FF"), // Dodger Blue
-        Color.Parse("#5D3FD3"), // Dark Purple
-        Color.Parse("#4B0082") // Indigo
+        Color.Parse("#A9A9A9"), // Medium Gray
+        Color.Parse("#C3BEBE"), // Pastel Slate
+        Color.Parse("#BEBEBE"), // Soft Gray
+        Color.Parse("#E6D0D8"), // Pastel Thistle
+        Color.Parse("#FFB6C1"), // Light Pink
+        Color.Parse("#C96F6F"), // Soft Blood Red
+        Color.Parse("#C86464"), // Pastel Dark Red
+        Color.Parse("#F9A76C"), // Pastel Orange
+        Color.Parse("#FBE89F"), // Pastel Gold
+        Color.Parse("#FFF799"), // Pastel Yellow
+        Color.Parse("#A8D5BA"), // Pastel Green
+        Color.Parse("#B2DAD2"), // Pastel Sea Green
+        Color.Parse("#9FD5DB"), // Pastel Turquoise
+        Color.Parse("#A4C8EF"), // Pastel Blue
+        Color.Parse("#B5A5E9"), // Pastel Purple
+        Color.Parse("#A89FCF")  // Pastel Indigo
     ];
 
     public List<DisplayScoreModel> DisplayScore { get; set; } = new()
