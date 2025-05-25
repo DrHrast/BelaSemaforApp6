3cc81ee630b7ddfc86fc18825e76910fbea2b307
DrHrast <DrHrast@Omen>
2025-05-25 20:48:01 +0200

    Touchups to labels

--- BelaSemaforApp6/Models/TeamModel.cs
+++ BelaSemaforApp6/Models/TeamModel.cs
@@ -5,4 +5,6 @@ public class TeamModel
     public string? Name { get; set; } = "Team";
     public string? FirstPlayer { get; set; }
     public string? SecondPlayer { get; set; }
+
+    public string? DisplayPlayers => $"{FirstPlayer}, {SecondPlayer}";
 }
